d5b1ab8b464ed79b890e48c84f69a7e6b0701da8
shavit <shavit@users.noreply.github.com>
2016-10-18 22:49:33 -0400

    🎀

---
+++ WebCam/PlayerController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerController.swift
+//  WebCam
+//
+//  Created by Shavit Tzuriel on 10/18/16.
+//  Copyright © 2016 Shavit Tzuriel. All rights reserved.
+//
+
+import Cocoa
+import AVKit
+
+class PlayerController: AVPlayerView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
