0fc92245ef94d9f9c0749774a64541c592c9545f
shavit <shavit@users.noreply.github.com>
2016-10-18 22:43:13 -0400

    🎀

---
+++ WebCam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebCam
+//
+//  Created by Shavit Tzuriel on 10/18/16.
+//  Copyright © 2016 Shavit Tzuriel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebCam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebCam
+//
+//  Created by Shavit Tzuriel on 10/18/16.
+//  Copyright © 2016 Shavit Tzuriel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
