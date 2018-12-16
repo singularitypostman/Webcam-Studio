47ae99e1df780773511710cf29a2fce08d6c3c40
shavit <shavit@users.noreply.github.com>
2018-12-16 15:35:38 -0500

    Use a default URL for the stream

--- Monique/MainViewController.swift
+++ Monique/MainViewController.swift
@@ -20,6 +20,14 @@ class MainViewController: NSSplitViewController {
   @IBAction func startStreaming(_ sender: Any){
     debugPrint("[MainViewController]", "Toggle. Is streaming?", isStreaming)
     
+    if isStreaming {
+      // TODO: Stop the stream
+    } else {
+      let hostname: String = ProcessInfo.processInfo.environment["RTMP_HOST"] ?? "127.0.0.1"
+      let url = "rtmp://<server ip>:1935/stream/dev_stream"
+      debugPrint("[MainViewController]", "Starting a stream to ", url)
+    }
+    
     isStreaming = !isStreaming
   }
   
