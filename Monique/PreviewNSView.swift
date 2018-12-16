cf04e323bc4f374f23a8a767460a80d308610d8b
shavit <shavit@users.noreply.github.com>
2018-12-16 17:31:20 -0500

    Delegate the preview to capture audio and video

--- Monique/PreviewNSView.swift
+++ Monique/PreviewNSView.swift
@@ -12,16 +12,38 @@
 import Cocoa
 import AVFoundation
 
-class PreviewNSView: NSView {
+class PreviewNSView: NSView,
+  AVCaptureVideoDataOutputSampleBufferDelegate,
+  AVCaptureAudioDataOutputSampleBufferDelegate {
   
   private var session: AVCaptureSession? = nil
   private var previewLayer: AVCaptureVideoPreviewLayer? = nil
   
   fileprivate let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+  //fileprivate let queueWriter = DispatchQueue(label: "PreviewNSView.Writer")
+  
+  fileprivate var bufWriter: AVAssetWriterInput? = nil
   
   override func draw(_ dirtyRect: NSRect) {
+    super.draw(dirtyRect)
+  
     startPreview()
   }
+  
+  // MARK: AVFoundation
+  
+  func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    // bufWriter?.append(sampleBuffer)
+  }
+
+  func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    guard let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+    let imageSize = CVImageBufferGetEncodedSize(imageBuffer)
+    
+    debugPrint("[PreviewNSView]", "captureOutput.didDrop", imageSize)
+  }
+  
+  // MARK: Video preview
   
   /**
    Display the preview from the webcam
@@ -45,7 +67,9 @@ class PreviewNSView: NSView {
     }
     
     let videoOutput: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+    videoOutput.setSampleBufferDelegate(self, queue: queue)
     let audioOutput: AVCaptureAudioDataOutput = AVCaptureAudioDataOutput()
+    audioOutput.setSampleBufferDelegate(self, queue: queue)
     session?.sessionPreset = AVCaptureSession.Preset.high
     session?.addOutput(videoOutput)
     session?.addOutput(audioOutput)
@@ -53,9 +77,15 @@ class PreviewNSView: NSView {
     guard session != nil else { return }
     previewLayer = AVCaptureVideoPreviewLayer(session: session!)
     layer = previewLayer
+
+    bufWriter = AVAssetWriterInput(mediaType: .video, outputSettings: [
+      AVVideoCodecKey: AVVideoCodecH264,
+      AVVideoWidthKey: 1280,
+      AVVideoHeightKey: 720,
+      ])
     
     queue.async { [weak self] in
-      self?.previewLayer?.session?.startRunning()
+      self?.session?.startRunning()
     }
   }
 }
