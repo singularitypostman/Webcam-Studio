33837f8838c34372eeac3670805f2213ab7cdfd5
shavit <shavit@users.noreply.github.com>
2019-01-10 11:30:36 -0500

    Add preferences window

--- Monique/MainWindowController.swift
+++ Monique/MainWindowController.swift
@@ -9,15 +9,14 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
-
+    
     @IBOutlet var mainWindow: NSWindow!
     
     override func windowDidLoad() {
         super.windowDidLoad()
-    
+        
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         mainWindow.isMovableByWindowBackground = true
         mainWindow.titlebarAppearsTransparent = true
     }
-
 }
