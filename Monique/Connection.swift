bc88f57cc031016080c2372ca3371e3c2226a185
shavit <shavit@users.noreply.github.com>
2019-01-22 03:24:35 -0500

    Create a socket

--- Monique/Connection.swift
+++ Monique/Connection.swift
@@ -6,9 +6,27 @@
 //  Copyright © 2019 Shavit Tzuriel. All rights reserved.
 //
 
-import Foundation
+import CFNetwork
 
 class Connection {
+    
+    private var sockfd: CFSocket?
+    
+    init(ddr: String, port: uint) {
+        self.sockfd = CFSocketCreate(nil, AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, nil, nil)
+        /*
+        if sockfd == nil {
+            // TOOD: Add error here
+            // TODO: Remove this
+            fatalError("Could not initialize a Connection. Error creating a socket")
+            // return
+        }
+         */
+        
+//        struct servAddr sockaddr_in
+//        memset(&servAddr, 0, sizeof(servAddr))
+//        servAddr.sin_len
+    }
     
     /**
     Write to the socket
