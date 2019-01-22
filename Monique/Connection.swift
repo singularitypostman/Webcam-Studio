e6a3946a905b754856cb2eb0092d01779dec76d7
shavit <shavit@users.noreply.github.com>
2019-01-21 21:59:31 -0500

    Add a connection

---
+++ Monique/Connection.swift
@@ -0,0 +1,22 @@
+//
+//  Connection.swift
+//  Monique
+//
+//  Created by Shavit Tzuriel on 1/21/19.
+//  Copyright © 2019 Shavit Tzuriel. All rights reserved.
+//
+
+import Foundation
+
+class Connection {
+    
+    /**
+    Write to the socket
+     */
+    func write() { }
+    
+    /**
+     Receive from the socket
+     */
+    func receive() { }
+}
