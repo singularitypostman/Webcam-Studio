d97b1f7150b542db32e6ec34c779ba1c9dfdca11
shavit <shavit@users.noreply.github.com>
2019-02-02 15:54:42 -0500

    Manage the stream from the capture manager

--- Monique/MainViewController.swift
+++ Monique/MainViewController.swift
@@ -11,36 +11,8 @@ import AVFoundation
 
 class MainViewController: NSSplitViewController {
     
-    var streamClient: StreamClient? = nil
-    
     @IBOutlet var previewView: PreviewNSView!
     
     @IBOutlet weak var mBtnStream: NSButton!
     @IBOutlet weak var mLabelStatus: NSTextField!
-    
-    fileprivate var isStreaming = false
-    
-    // Stream to server
-    @IBAction func startStreaming(_ sender: Any){
-        debugPrint("[MainViewController]", "Toggle. Is streaming?", isStreaming)
-        
-        //@IBOutlet var previewView: PreviewNSView!
-        
-        //@IBOutlet var mBtnStream: NSButton!
-        
-        isStreaming = !isStreaming
-    }
-    
-    // MARK: Streaming
-    
-    fileprivate func startStreaming(){
-        // TODO: Replace the environment variable with user settings
-        //let hostname: String = ProcessInfo.processInfo.environment["RTMP_HOST"] ?? "127.0.0.1"
-        let hostname: String = "127.0.0.1"
-        let url = "rtmp://\(hostname):1935/live/dev_stream"
-        streamClient = StreamClient(address: url)
-        let err = streamClient?.publishStream()
-        // TODO: Remove this
-        debugPrint("[MainViewController]", "Publish stream:", err)
-    }
 }
--- Monique/PreviewNSView.swift
+++ Monique/PreviewNSView.swift
@@ -13,81 +13,110 @@ import Cocoa
 import AVFoundation
 
 class PreviewNSView: NSView,
-  AVCaptureVideoDataOutputSampleBufferDelegate,
-  AVCaptureAudioDataOutputSampleBufferDelegate {
-  
-  private var session: AVCaptureSession? = nil
-  private var previewLayer: AVCaptureVideoPreviewLayer? = nil
-  
-  fileprivate let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
-  //fileprivate let queueWriter = DispatchQueue(label: "PreviewNSView.Writer")
-  
-  fileprivate var bufWriter: AVAssetWriterInput? = nil
-  
-  override func draw(_ dirtyRect: NSRect) {
-    super.draw(dirtyRect)
-  
-    startPreview()
-  }
-  
-  // MARK: AVFoundation
-  
-  func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-    debugPrint("[PreviewNSView]", "captureOutput.didOutput", sampleBuffer)
-    // bufWriter?.append(sampleBuffer)
-  
-  }
-
-  func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-    guard let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-    let imageSize = CVImageBufferGetEncodedSize(imageBuffer)
+    AVCaptureVideoDataOutputSampleBufferDelegate,
+AVCaptureAudioDataOutputSampleBufferDelegate {
     
-    debugPrint("[PreviewNSView]", "captureOutput.didDrop", imageSize)
-  }
-  
-  // MARK: Video preview
-  
-  /**
-   Display the preview from the webcam
-   */
-  func startPreview(){
-    session?.stopRunning()
-    session = AVCaptureSession()
+    private var session: AVCaptureSession? = nil
+    private var previewLayer: AVCaptureVideoPreviewLayer? = nil
     
-    let webcamVideo: AVCaptureDevice? = AVCaptureDevice.devices(for: .video)[0]
-    let webcamAudio: AVCaptureDevice? = AVCaptureDevice.devices(for: .audio)[0]
+    fileprivate var streamClient: StreamClient? = nil
+    fileprivate var isStreaming: Bool = false
     
-    // Add video input
-    do{
-      let videoInput: AVCaptureDeviceInput = try AVCaptureDeviceInput(device: webcamVideo!)
-      session?.addInput(videoInput)
-      
-      let audioInput: AVCaptureDeviceInput = try AVCaptureDeviceInput(device: webcamAudio!)
-      session?.addInput(audioInput)
-    } catch let err as NSError{
-      print("Error adding device: \(err)")
+    fileprivate let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+    //fileprivate let queueWriter = DispatchQueue(label: "PreviewNSView.Writer")
+    
+    fileprivate var bufWriter: AVAssetWriterInput? = nil
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        startPreview()
     }
     
-    let videoOutput: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
-    videoOutput.setSampleBufferDelegate(self, queue: queue)
-    let audioOutput: AVCaptureAudioDataOutput = AVCaptureAudioDataOutput()
-    audioOutput.setSampleBufferDelegate(self, queue: queue)
-    session?.sessionPreset = AVCaptureSession.Preset.high
-    session?.addOutput(videoOutput)
-    session?.addOutput(audioOutput)
+    // MARK: AVFoundation
     
-    guard session != nil else { return }
-    previewLayer = AVCaptureVideoPreviewLayer(session: session!)
-    layer = previewLayer
-
-    bufWriter = AVAssetWriterInput(mediaType: .video, outputSettings: [
-      AVVideoCodecKey: AVVideoCodecH264,
-      AVVideoWidthKey: 1280,
-      AVVideoHeightKey: 720,
-      ])
-    
-    queue.async { [weak self] in
-      self?.session?.startRunning()
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if isStreaming {
+            debugPrint("[PreviewNSView]", "captureOutput.didOutput", sampleBuffer)
+        }
+        // bufWriter?.append(sampleBuffer)
+        
     }
-  }
+    
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let imageBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        let imageSize = CVImageBufferGetEncodedSize(imageBuffer)
+        
+        //debugPrint("[PreviewNSView]", "captureOutput.didDrop", imageSize)
+    }
+    
+    // MARK: Video preview
+    
+    /**
+     Display the preview from the webcam
+     */
+    func startPreview(){
+        session?.stopRunning()
+        session = AVCaptureSession()
+        
+        let webcamVideo: AVCaptureDevice? = AVCaptureDevice.devices(for: .video)[0]
+        let webcamAudio: AVCaptureDevice? = AVCaptureDevice.devices(for: .audio)[0]
+        
+        // Add video input
+        do{
+            let videoInput: AVCaptureDeviceInput = try AVCaptureDeviceInput(device: webcamVideo!)
+            session?.addInput(videoInput)
+            
+            let audioInput: AVCaptureDeviceInput = try AVCaptureDeviceInput(device: webcamAudio!)
+            session?.addInput(audioInput)
+        } catch let err as NSError{
+            print("Error adding device: \(err)")
+        }
+        
+        let videoOutput: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: queue)
+        let audioOutput: AVCaptureAudioDataOutput = AVCaptureAudioDataOutput()
+        audioOutput.setSampleBufferDelegate(self, queue: queue)
+        session?.sessionPreset = AVCaptureSession.Preset.high
+        session?.addOutput(videoOutput)
+        session?.addOutput(audioOutput)
+        
+        guard session != nil else { return }
+        previewLayer = AVCaptureVideoPreviewLayer(session: session!)
+        layer = previewLayer
+        
+        bufWriter = AVAssetWriterInput(mediaType: .video, outputSettings: [
+            AVVideoCodecKey: AVVideoCodecType.h264,
+            AVVideoWidthKey: 1280,
+            AVVideoHeightKey: 720,
+            ])
+        
+        queue.async { [weak self] in
+            self?.session?.startRunning()
+        }
+    }
+    
+    // MARK: Streaming Client
+    
+    // Stream to server
+    @IBAction func startStreaming(_ sender: Any){
+        debugPrint("[MainViewController]", "Toggle. Is streaming?", isStreaming)
+        
+        // TODO: Replace the environment variable with user settings
+        //let hostname: String = ProcessInfo.processInfo.environment["RTMP_HOST"] ?? "127.0.0.1"
+        let hostname: String = "127.0.0.1"
+        let url = "rtmp://\(hostname):1935/live/dev_stream"
+        streamClient = StreamClient(address: url)
+        let err = streamClient?.publishStream()
+        // TODO: Remove this
+        debugPrint("[PreviewNSView]", "Publish stream:", Int32(err))
+        
+        //@IBOutlet var previewView: PreviewNSView!
+        
+        //@IBOutlet var mBtnStream: NSButton!
+        
+        isStreaming = !isStreaming
+    }
+    
 }
